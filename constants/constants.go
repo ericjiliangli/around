142bdddbf4895cdb01e84e02c770b784a107fbbf
Eric <jiliangli0904@gmail.com>
2022-08-19 22:59:01 -0500

    Initial update

---
+++ constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	USER_INDEX  = "user"
+	POST_INDEX  = "post"
+	ES_URL      = "http://10.128.0.2:9200"
+	ES_USERNAME = "jiliangli0904"
+	ES_PASSWORD = "jilkjilkj"
+	GCS_BUCKET = "eric-around"
+)
